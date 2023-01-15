b92d09594c7a60c3cfd873717c5f06c6b5f51c4b
alvinthosatria <alvinthosatria@gmail.com>
2023-01-15 16:25:11 +0800

    commit auth.js controller

--- api/index.js
+++ api/index.js
@@ -1,8 +1,25 @@
 import express from "express";
-
 const app = express();
+//each route has its own controllers (i.e. user.js is the controllers of users.js)
+import userRoutes from "./routes/users.js"
+import postRoutes from "./routes/posts.js"
+import likeRoutes from "./routes/likes.js"
+import commentRoutes from "./routes/comments.js"
+import authRoutes from "./routes/auth.js"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
-app.use(express.json());
+//middlewares
+app.use(express.json()); //to send json objects
+app.use(cors()); //cors is used so that only the localhost:3000 URL can access the API
+app.use(cookieParser());
+
+//whenever we go to that page, we will go to userRoutes which have different endpoints
+app.use("/api/users", userRoutes) 
+app.use("/api/posts", postRoutes) 
+app.use("/api/likes",likeRoutes)
+app.use("/api/comments", commentRoutes)
+app.use("/api/auth",authRoutes) 
 
 app.listen(8800, () => {
     console.log("API working!");
