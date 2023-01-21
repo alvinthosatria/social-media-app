fc4caf3a0fcd66c6dffd4b52e4d58742a6e0cc35
alvinthosatria <alvinthosatria@gmail.com>
2023-01-21 14:37:59 +0800

    fetch comments

--- client/src/components/comments/Comments.jsx
+++ client/src/components/comments/Comments.jsx
@@ -3,35 +3,19 @@ import "./comments.scss";
 import { AuthContext } from "../../context/authContext";
 import { useQuery } from '@tanstack/react-query';
 import { makeRequest } from "../../axios";
+import moment from "moment";
 
-const Comments = () => {
+const Comments = ({ postId }) => {
   const { currentUser } = useContext(AuthContext);
+
+  //fetch comments
   const { isLoading, error, data } = useQuery(['comments'], () =>
-    makeRequest.get("/comments").then((res) => {
+    makeRequest.get("/comments?postId="+postId).then((res) => {
       return res.data;
     })
   );
-  //Temporary
-  // const comments = [
-  //   {
-  //     id: 1,
-  //     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam",
-  //     name: "John Doe",
-  //     userId: 1,
-  //     profilePicture:
-  //       "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-  //   },
-  //   {
-  //     id: 2,
-  //     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam",
-  //     name: "Jane Doe",
-  //     userId: 2,
-  //     profilePicture:
-  //       "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
-  //   },
-  // ];
 
-
+  console.log(data)
 
   return (
     <div className="comments">
@@ -40,14 +24,16 @@ const Comments = () => {
         <input type="text" placeholder="write a comment" />
         <button>Send</button>
       </div>
-      {comments.map((comment) => (
+      {isLoading 
+      ? "loading" 
+      : data.map((comment) => (
         <div className="comment">
           <img src={comment.profilePicture} alt="" />
           <div className="info">
             <span>{comment.name}</span>
             <p>{comment.desc}</p>
           </div>
-          <span className="date">1 hour ago</span>
+          <span className="date">{moment(comment.createdAt.fromNow())}</span>
         </div>
       ))}
     </div>
